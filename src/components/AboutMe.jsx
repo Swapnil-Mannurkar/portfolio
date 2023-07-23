9a2911549a670149b9820e1cf39bf36fc40413ce
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-07-23 14:05:19 +0530

    first commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import './AboutMe.css'
-import AboutImg from '../images/About.jpg'
+import Img from '../images/About.jpg'
 
 const AboutMe = () => {
-    return (
-        <div className='AMContainer'>
-            <div className='AMImgContainer'>
-                <img src={AboutImg} />
-            </div>
-            <div className='AMInfoContainer'>
-                <h1>ABOUT</h1>
-                <h2>I'm a frontend web developer</h2>
-                <div className='AMInfoContent'>
-                    <p>I'm a passionate developer who loves programming and I'm intrested in full stack web development and software development. I have experience working with HTML, CSS, JavaScript, MySQL, PHP, WordPress and Python.</p>
-                    <p>I have worked as a freelancer for an year and I am currently working at TokSkill as a frontend web developer - Intern</p>
-                </div>
-            </div>
+  return (
+    <div className='AMContainer'>
+      <div className='AMImgCont'>
+
+      </div>
+      <div className='AMInfoCont'>
+        <h1>ABOUT</h1>
+        <h2>I'm a frontend web developer</h2>
+        <div className='AMInfoContent'>
+          <p>I'm a passionate developer who loves programming and I'm intrested in full stack web development and software development. I have experience working with HTML, CSS, JavaScript, MySQL, PHP, WordPress and Python.</p>
+          <p>I have worked as a freelancer for an year and I am currently working at TokSkill as a frontend web developer - Intern</p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default AboutMe
