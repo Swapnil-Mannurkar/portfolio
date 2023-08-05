19baf33da7731f10e6e7d5e3aaced747fd17ac9c
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-08-05 15:56:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import ContactMe from './components/ContactMe';
 
-//test
-
 function App() {
   return (
     <div>
