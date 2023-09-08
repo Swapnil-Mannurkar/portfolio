de0298ade9cc09fd118aed46841ed4484d27e792
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-09-08 23:41:52 +0530

    commit

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,69 +1,72 @@
-import React from 'react'
-import './HeroSection.css'
-import myImage from '../../images/Swapnil.png'
-import { useEffect, useRef } from 'react';
+import React from "react";
+import "./HeroSection.css";
+import myImage from "../../images/Swapnil.png";
+import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
-    const words = [
-        'Frontend Developer',
-        'MySQL',
-        'Python',
-        'C/C++'
-    ];
+  const words = ["Frontend Developer", "MySQL", "Python", "C/C++"];
 
-    const textRef = useRef();
-    const typingSpeed = 50;
-    const waitTime = 2000;
-    const erasingSpeed = 50;
+  const textRef = useRef();
+  const typingSpeed = 50;
+  const waitTime = 2000;
+  const erasingSpeed = 50;
 
-    const writeText = async (word) => {
-        for (let i = 0; i <= word.length; i++) {
-            await new Promise((resolve) => setTimeout(resolve, typingSpeed));
-            textRef.current.textContent = word.substring(0, i);
-        }
+  const writeText = async (word) => {
+    for (let i = 0; i <= word.length; i++) {
+      await new Promise((resolve) => setTimeout(resolve, typingSpeed));
+      textRef.current.textContent = word.substring(0, i);
+    }
+  };
+
+  const eraseText = async (word) => {
+    for (let i = word.length; i >= 0; i--) {
+      await new Promise((resolve) => setTimeout(resolve, erasingSpeed));
+      textRef.current.textContent = word.substring(0, i);
+    }
+  };
+
+  const changeText = async () => {
+    for (let i = 0; i < words.length; i++) {
+      const currentWord = words[i];
+
+      await writeText(currentWord);
+      await new Promise((resolve) => setTimeout(resolve, waitTime));
+      await eraseText(currentWord);
+    }
+    changeText();
+  };
+
+  useEffect(() => {
+    changeText();
+    return () => {
+      clearTimeout();
     };
+  });
 
-    const eraseText = async (word) => {
-        for (let i = word.length; i >= 0; i--) {
-            await new Promise((resolve) => setTimeout(resolve, erasingSpeed));
-            textRef.current.textContent = word.substring(0, i);
-        }
-    };
+  return (
+    <div className="HScontainer">
+      <div className="HSPhoto">
+        <img src={myImage} alt="Swapnil Mannurkar"></img>
+      </div>
+      <p className="HSName">
+        <span>Hi, I'm Swapnil Mannurkar</span>👋
+      </p>
+      <div className="HSIntro">
+        Cyberjunkie |&nbsp;<p ref={textRef}></p>
+      </div>
+      <div className="HSButton">
+        <button>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/1Ct1ZngaH523RN9ftK3ZFBLD6WW588pMr/view?usp=sharing"
+          >
+            Download CV ↗
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    const changeText = async () => {
-        for (let i = 0; i < words.length; i++) {
-            const currentWord = words[i];
-
-            await writeText(currentWord);
-            await new Promise((resolve) => setTimeout(resolve, waitTime));
-            await eraseText(currentWord);
-        }
-        changeText();
-    };
-
-    useEffect(() => {
-        changeText();
-        return () => {
-            clearTimeout();
-        };
-    });
-
-    return (
-        <div className='HScontainer'>
-            <div className='HSPhoto'>
-                <img src={myImage} alt='Swapnil Mannurkar'></img>
-            </div>
-            <p className='HSName'>
-                <span>Hi, I'm Swapnil Mannurkar</span>👋
-            </p>
-            <div className='HSIntro'>
-                Cyberjunkie |&nbsp;<p ref={textRef}></p>
-            </div>
-            <div className='HSButton'>
-                <button><a target='_blank' rel='noreferrer' href='https://drive.google.com/file/d/15Dl0BMtbLfUmEsq5rmnOVYyaipVJObp1/view?usp=sharing'>Download CV ↗</a></button>
-            </div>
-        </div>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
