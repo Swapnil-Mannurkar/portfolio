d9152578298556c66bbdf4b6e62835b1dc02cb33
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-09-23 20:30:41 +0530

    commit

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react'
-import './AboutMe.css'
+import React, { useState, useRef, useEffect } from "react";
+import "./AboutMe.css";
 
 const AboutMe = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -15,7 +15,7 @@ const AboutMe = () => {
   useEffect(() => {
     const observer = new IntersectionObserver(handleIntersection, {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.4,
     });
 
@@ -45,19 +45,37 @@ const AboutMe = () => {
   }, [isVisible]);
 
   return (
-    <div className='AMContainer' id="about-me-section">
-      <div className={`AMImgCont ${isVisible ? 'fade-in' : ''}`} ref={ref}>
-      </div>
-      <div className={`AMInfoCont ${isVisible ? 'slide-left' : ''}`} ref={reff}>
+    <div className="AMContainer" id="about-me-section">
+      <div
+        className={`AMImgCont ${isVisible ? "fade-in" : ""}`}
+        ref={ref}
+      ></div>
+      <div className={`AMInfoCont ${isVisible ? "slide-left" : ""}`} ref={reff}>
         <h1>ABOUT</h1>
         <h2>Frontend Web Developer</h2>
-        <div className='AMInfoContent'>
-          <p>I'm a passionate developer who loves programming and I'm intrested in full stack web development and software development. I have experience working with HTML, CSS, JavaScript, MySQL, PHP, WordPress and Python.</p>
-          <p>I have worked as a frontend web developer both as a freelancer and at <a href='https://www.tokskill.com' rel='noreferrer' target='_blank' style={{ textDecoration: 'none', color: 'lightblue' }}>TokSkill</a> as an intern.</p>
+        <div className="AMInfoContent">
+          <p>
+            I'm a passionate developer who loves programming and I'm intrested
+            in full stack web development and software development. I have
+            experience working with React.js, Redux, Context API, Next.js,
+            Javascript, CSS, HTML, MySQL, Python.
+          </p>
+          <p>
+            I have worked as a frontend developer for{" "}
+            <a
+              href="https://www.tokskill.com"
+              rel="noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none", color: "lightblue" }}
+            >
+              TokSkill
+            </a>{" "}
+            and as a freelancer.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/components/experience/ExpCard.jsx
+++ src/components/experience/ExpCard.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import './ExpCard.css'
-import { FaGlobe, FaLaptop, FaNetworkWired } from 'react-icons/fa'
+import React from "react";
+import "./ExpCard.css";
+import { FaGlobe, FaLaptop, FaNetworkWired } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const iconStyle = {
-    fontSize: '40px',
-    width: '25%',
-    alignself: 'center'
+  fontSize: "40px",
+  width: "25%",
+  alignself: "center",
 };
 
 const ExpCard = (props) => {
-    return (
-        <div className='ExpCardCont'>
-            <div className='iconDivStyle'>
-                {getIcon(props.icon)}
-            </div>
-            <h4 className='ExpHead'>{props.title}</h4>
-            <p className='ExpDetail'>{props.details}</p>
-        </div >
-    )
-}
+  return (
+    <div className="ExpCardCont">
+      <div className="iconDivStyle">{getIcon(props.icon)}</div>
+      <h4 className="ExpHead">{props.title}</h4>
+      <p className="ExpDetail">{props.details}</p>
+    </div>
+  );
+};
 
-export default ExpCard
+export default ExpCard;
 
 const getIcon = (iconName) => {
-    switch (iconName) {
-        case 'FaLaptop':
-            return <FaLaptop style={iconStyle} />;
-        case 'FaNetworkWired':
-            return <FaNetworkWired style={iconStyle} />;
-        case 'FaGlobe':
-            return <FaGlobe style={iconStyle} />;
-        default:
-            return null;
-    }
-}
+  switch (iconName) {
+    case "FaLaptop":
+      return <FaLaptop style={iconStyle} />;
+    case "FaNetworkWired":
+      return <FaNetworkWired style={iconStyle} />;
+    case "FaGlobe":
+      return <FaGlobe style={iconStyle} />;
+    case "FaHuman":
+      return <FontAwesomeIcon icon={faUser} style={iconStyle} />;
+    default:
+      return null;
+  }
+};
